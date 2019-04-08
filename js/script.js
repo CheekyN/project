d8b87a7dcc41a7a370bdb2d570e57ba8863d0e7e
CheekyN <48863617+CheekyN@users.noreply.github.com>
2019-04-08 13:20:43 +0000

    to sho nado
    tolko raboet ne tak

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const decCache = [],
+    decCases = [2, 0, 1, 1, 1, 2];
+
+function decOfNum(number, titles) {
+    if (!decCache[number]) decCache[number] = number % 100 > 4 && number % 100 < 20 ? 2 : decCases[Math.min(number % 10, 5)];
+    return titles[decCache[number]];
+}
+
+
+function countdown() {
+    setInterval(function() {
+        const hoursLeft = document.getElementById('timer__hours');
+        const hoursText = document.getElementById('hours');
+        const minutesLeft = document.getElementById('timer__minutes');
+        const minutesText = document.getElementById('minutes');
+        const secLeft = document.getElementById('timer__seconds');
+        const secText = document.getElementById('seconds');
+        const dateNow = new Date().getTime();
+        // dateNow.setMilliseconds(dateNow.getMilliseconds());
+        const dateFinish = new Date().getTime() + 86400000;
+        // dateFinish.setMilliseconds(dateFinish.getMilliseconds() + 86400000);
+        const sec = Math.floor((dateFinish - dateNow) / 1000);
+        const secToMinutes = sec % 3600;
+        const hoursNumber = Math.floor(sec / 3600);
+        const minutesNumber = Math.floor(secToMinutes / 60);
+        const secondsNumber = secToMinutes % 60;
+        hoursText.innerHTML = decOfNum(hoursNumber, ['час', 'часа', 'часов']);
+        minutesText.innerHTML = decOfNum(minutesNumber, ['минута', 'минуты', 'минут']);
+        secText.innerHTML = decOfNum(secondsNumber, ['секунда', 'секунды', 'секунд']);
+        hoursLeft.innerHTML = hoursNumber;
+        minutesLeft.innerHTML = minutesNumber;
+        secLeft.innerHTML = secondsNumber;
+
+    },1000)
+}
+
+countdown();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
